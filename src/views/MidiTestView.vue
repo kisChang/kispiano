03794966984a8ff91052d4adc2c36555b01ad791
KisChang <734615869@qq.com>
2020-06-29 20:57:27 +0800

    完善midi测试功能

--- src/views/MidiTestView.vue
+++ src/views/MidiTestView.vue
@@ -2,9 +2,9 @@
     <div>
         <h3>Keep it Simple</h3>
         <div>{{midiMsg}}</div>
-        <v-btn @click="playOut('C3')">C3</v-btn>
-        <v-btn @click="playOut('D#3')">D#3</v-btn>
-        <v-btn @click="playOut('G3')">G3</v-btn>
+        <b-button @click="playOut([144, 60, 100])">C3</b-button>
+        <b-button @click="playOut([144, 60, 50])">D#3</b-button>
+        <b-button @click="playOut([144, 60, 127])">G3</b-button>
         <div>
             <div v-for="item in loggerText" :key="item">
                 {{item}}
@@ -16,11 +16,7 @@
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
     import PianoKeyboard from '@/components/PianoKeyboard.vue';
-
-    //必须导入这个，才能启用WebMidi
-    // import 'web-midi-api';
-    //文档 https://github.com/djipco/webmidi
-    //import WebMidi, {Input, InputEventNoteon, Output} from "webmidi";
+    import "bootstrap-vue/src/components/modal/index";
 
     @Component({
         components: {PianoKeyboard}
@@ -28,49 +24,27 @@
     export default class HelloWorld extends Vue {
         loggerText: [string] = [''];
         midiMsg = '';
+        midi: any;
         midiIn: any;
         midiOut: any;
 
         mounted(): void {
-            /*WebMidi.enable((err) => {
-                if (err) {
-                    this.midiMsg = "WebMidi could not be enabled." + JSON.stringify(err);
-                } else {
-                    this.midiMsg = "WebMidi enabled!";
-
-                    window["WebMidi"] = WebMidi;
-
-                    this.midiIn = WebMidi.inputs[0];
-                    this.midiOut = WebMidi.outputs[0];
-
-                    if (this.midiIn) {
-                        this.midiIn.addListener("noteon", "all", (event: InputEventNoteon) => {
-                            this.addLog(String(event));
-                        })
-                    } else {
-                        this.addLog("this.midiIn is null");
-                    }
-                }
-            });*/
-
-            // navigator.requestMIDIAccess().then(this.onMIDISuccess, this.onMIDIFailure);
-            }
-            onMIDIFailure(msg: any){
-                console.log('Failed to get MIDI access - ' + msg);
-            }
-            onMIDISuccess(midiAccess: any){
-                this.midiIn = midiAccess.inputs;
-                this.midiOut = midiAccess.outputs;
-                //window['midiOut'] = this.midiOut;
-            }
-
-        playOut(note: string){
-            if (this.midiOut){
-                //this.midiOut.playNote(note);
-                this.midiOut.forEach(function(port: any){
-                    port.send([0x80, 60, 0]);
+            if (!('requestMIDIAccess' in navigator)) {
+                this.$bvModal.msgBoxOk('暂不支持此设备！', {centered: true});
+            } else {
+                //@ts-ignore
+                navigator.requestMIDIAccess().then((midi: any) => {
+                    this.midi = midi;
+                    this.midiIn = this.midi.inputs.values().next().value;
+                    this.midiOut = this.midi.outputs.values().next().value;
                 });
-            }else {
+            }
+        }
+
+        playOut(note: []) {
+            if (this.midiOut) {
+                this.midiOut.send(note)
+            } else {
                 this.addLog("this.midi Out is null");
             }
         }
