6289dd6fcd4e53ed29a6fe24bdd7e37baa4460f7
KisChang <734615869@qq.com>
2020-06-30 21:39:10 +0800

    加入axios 暂未测试

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/axios'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//@ts-ignore
+window['API_PATH'] = 'http://localhost:8081/';
 
 new Vue({
   router,
