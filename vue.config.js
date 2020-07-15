161468b9864506f2e633c5000d8cf261b5b07d8e
KisChang <734615869@qq.com>
2020-07-15 13:54:58 +0800

    dev bak build config 混淆

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,46 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const JavaScriptObfuscator = require('webpack-obfuscator');
+
 module.exports = {
-  pwa: {
-    name: 'Kis Piano',
-    themeColor: '#fafafa',
-    msTileColor: '#fafafa',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    manifestOptions: {
-      display: 'standalone',
+    pwa: {
+        name: 'Kis Piano',
+        themeColor: '#fafafa',
+        msTileColor: '#fafafa',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        manifestOptions: {
+            display: 'standalone',
+        },
+        workboxOptions: {
+            skipWaiting: true,
+        },
     },
-    workboxOptions: {
-      skipWaiting: true,
+    devServer: {
+        port: 10070,
+        disableHostCheck: true,
     },
-  },
-  devServer: {
-    port: 10070,
-    disableHostCheck: true,
-  },
+    configureWebpack: (config) => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置
+            config.mode = 'production';
+
+            // 代码混淆
+            config.plugins.splice(1, 0, new JavaScriptObfuscator({
+                compact: true,
+                rotateStringArray: true,
+                identifierNamesGenerator: 'hexadecimal',
+                stringArray: true,
+                debugProtection: true, /*禁用开发者工具*/
+                selfDefending: false, /*无法开启，启动白屏*/
+                stringArrayEncoding: false,  /*无法开启，编译过程一直报错：URIError: URI malformed*/
+                stringArrayThreshold: 0.8,
+                log: false,
+                target: "browser"
+            }));
+            /*config.plugins.push();*/
+        } else {
+            // 为开发环境修改配置
+            config.mode = 'development';
+        }
+    },
 };
