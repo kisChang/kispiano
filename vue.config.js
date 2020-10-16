7cca1f8574df4928bd308f99c0749c58005dfd66
KisChang <734615869@qq.com>
2020-10-16 16:54:01 +0800

    更新支持 cdn

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const TerserPlugin = require('terser-webpack-plugin')
 
+const cdn = {
+    css: [],
+    js: [
+        'https://cdn.bootcdn.net/ajax/libs/core-js/3.6.5/minified.min.js',
+        'https://cdn.bootcss.com/vue/2.6.10/vue.runtime.min.js',
+        'https://cdn.bootcss.com/vue-router/3.2.0/vue-router.min.js',
+        'https://cdn.bootcdn.net/ajax/libs/bootstrap-vue/2.10.0/bootstrap-vue.min.js'
+    ]
+};
+
 module.exports = {
     productionSourceMap: false,
     pwa: {
@@ -20,31 +30,47 @@ module.exports = {
         port: 10070,
         disableHostCheck: true,
     },
-    configureWebpack: (config) => {
+    chainWebpack: config => {
+        // 注入cdn
+        config.plugin('html').tap(args => {
+            args[0].cdn = cdn;
+            return args;
+        });
+
         if (process.env.NODE_ENV === 'production') {
             // 为生产环境修改配置
             config.mode = 'production';
-
-            // 代码混淆
-            // 将每个依赖包打包成单独的js文件
-            const optimization = {
-                minimize: true,
-                minimizer: [new TerserPlugin({
-                    terserOptions: {
-                        warnings: false,
-                        compress: {
-                            // eslint-disable-next-line @typescript-eslint/camelcase
-                            drop_console: true, drop_debugger: true, pure_funcs: ['console.log']
-                        },
-                    }
-                })],
-            };
-            Object.assign(config, {
-                optimization
-            })
         } else {
             // 为开发环境修改配置
             config.mode = 'development';
         }
     },
+
+    configureWebpack: config => {
+        // 用cdn方式引入
+        config.externals = ['vue', 'vue-router', 'core-js', 'bootstrap-vue'];
+
+        //代码压缩
+        if (process.env.NODE_ENV === 'production') {
+            // 代码混淆
+            // 将每个依赖包打包成单独的js文件
+            const optimization = {
+                minimize: true,
+                minimizer: [new TerserPlugin({
+                    parallel: true,
+                    sourceMap: false,
+                    extractComments: false,
+                    terserOptions: {
+                        compress: {
+                            // eslint-disable-next-line @typescript-eslint/camelcase
+                            drop_console: true, drop_debugger: true, pure_funcs: ['console.log']
+                        }
+                    }
+                })],
+            };
+            Object.assign(config, {
+                optimization
+            });
+        }
+    }
 };
